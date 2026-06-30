56c806ec6e4cc7bf96b1845ae75db59f74a14227
João Lucas <164104520+Joaolucas013@users.noreply.github.com>
2026-06-30 11:42:14 -0300

    melhorando o estilo do botão, remoção do padding

--- project/src/app/animacoes.ts
+++ project/src/app/animacoes.ts
@@ -1,4 +1,3 @@
-
 import {
   animate,
   state,
@@ -7,30 +6,38 @@ import {
   trigger,
 } from '@angular/animations';
 
-
-
 export const aumentarBordaTrigger = trigger('aumentoDeBorda', [
-  state('default', style({
-    border: '1px solid #8d3434',
-  })),
-  state('aumentarBorda', style({
-    border: '3px solid #8d3434',
-    filter: 'brightness(92%)',
-    transform: 'scale(1.03)',
-  })),
+  state(
+    'default',
+    style({
+      border: '1px solid #8d3434',
+    }),
+  ),
+  state(
+    'aumentarBorda',
+    style({
+      border: '3px solid #8d3434',
+      filter: 'brightness(92%)',
+      transform: 'scale(1.03)',
+    }),
+  ),
   transition('default => aumentarBorda', animate('100ms ease-out')),
   transition('aumentarBorda => default', animate('200ms ease-in')),
 ]);
 
-
 export const estilizarBotao = trigger('estiloBotao', [
-  state('default', style({
-    padding:"6px"
-  })),
-  state('aumentoPadding', style({
-    padding: '8px',
-    backgroundColor: 'red',
-  })),
+  state(
+    'default',
+    style({
+    }),
+  ),
+  state(
+    'aumentoPadding',
+    style({
+    
+      backgroundColor: 'red',
+    }),
+  ),
   transition('default => aumentoPadding', animate('50ms ease-out')),
   transition('aumentoPadding => default', animate('50ms ease-out')),
 ]);
